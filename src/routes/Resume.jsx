d5ff62f35c02f10b623ac1ca5e548fe59009c317
Howard Wang <howrdw.work@gmail.com>
2025-01-09 22:11:35 +1100

    fixed

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div 
       style={{
-        height: '100vh',    // 父容器占满整个屏幕高度
+        height: '100vh',    
         display: 'flex', 
         flexDirection: 'column', 
         margin: 0,
@@ -12,10 +12,7 @@ const Resume = () => {
     >
 
     <div style={{ flex: 1 }}>
-      {/* <Navbar/> */}
       <embed src="/Portfolio/assets/resume.pdf" width="100%" height="100%" style={{ border: 'none' }}  />
-      {/* <a href="/assets/resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</a> */}
-      {/* <Footer/> */}
     </div>
     </div>
 
