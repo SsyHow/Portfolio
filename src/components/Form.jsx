6bdb71b57667b08768b660e3bf16cd0cf372ee49
Howard Wang <howrdw.work@gmail.com>
2025-01-10 20:59:35 +1100

    contact

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,48 @@
 import "./Form.css"
 
-import React from 'react'
+import React, { useRef, useState} from 'react'
+import emailjs from "emailjs-com"
 
 const Form = () => {
+  const form = useRef();
+  const [status, setStatus] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_howrdw",
+        "template_vjzg8ua",
+        form.current,
+        "l-yd9PBZBIyH9JIe3"
+      )
+      .then(
+        (result) => {
+          console.log('1')
+          setStatus("Message sent successfully")
+        },
+        (error) => {
+          console.log('0')
+          setStatus("Fail to send message")
+        }
+      )
+
+      e.target.reset()
+  }
+
   return <div className="form">
-    <form >
+    <form ref={form} onSubmit={sendEmail}>
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" required></input>
         <label>Email</label>
-        <input type="email"></input>
+        <input type="email" required></input>
         <label>Subject</label>
-        <input type="text"></input>
+        <input type="text" required></input>
         <label>Message</label>
-        <textarea row="6" placeholder="Type your message here"/>
-        <button className="btn">Submit</button>        
+        <textarea row="6" placeholder="Type your message here" required/>
+        <button className="btn">Submit</button>   
+        {status && <p>{status}</p>}     
     </form>
 
   </div>
